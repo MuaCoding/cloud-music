c7fd6f88e2c7ea483be495ae184cd187c3ce71dc
XiaoMingLi <1165406082@qq.com>
2020-03-19 23:38:43 +0800

    update

--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -6,6 +6,7 @@ import Swiper from "swiper";
 function Slider(props) {
   const [sliderSwiper, setSliderSwiper] = useState(null);
   const { bannerList } = props;
+
   useEffect(() => {
     if (bannerList.length && !sliderSwiper) {
       let sliderSwiper = new Swiper(".slider-container", {
@@ -18,11 +19,32 @@ function Slider(props) {
       });
       setSliderSwiper(sliderSwiper);
     }
-  },[bannerList.length,sliderSwiper]);
+  }, [bannerList.length, sliderSwiper]);
 
-  return(
-      <SliderContainer>
-        
-      </SliderContainer>
-  )
+  return (
+    <SliderContainer>
+      <div className="before"></div>
+      <div className="slider-container">
+        <div className="swiper-wrapper">
+          {bannerList.map(slider => {
+            return (
+              <div className="swiper-slide" key={slider.imageUrl}>
+                <div className="slider-nav">
+                  <img
+                    src={slider.imageUrl}
+                    width="100%"
+                    height="100%"
+                    alt="推荐"
+                  />
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <div className="swiper-pagination"></div>
+      </div>
+    </SliderContainer>
+  );
 }
+
+export default React.memo(Slider);
