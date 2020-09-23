d4434699d9d6787b4f20ecf993f6c98edcc1083b
lixm <lixm@126.com>
2020-09-23 17:04:38 +0800

    fix

--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -4,25 +4,21 @@ import "swiper/css/swiper.css";
 import Swiper from "swiper";
 
 function Slider(props) {
-  const [sliderSwiper, setSliderSwiper] = useState(null);
-  const { bannerList } = props;
-  useEffect(() => {
-    if (bannerList.length && !sliderSwiper) {
-      let sliderSwiper = new Swiper(".slider-container", {
-        loop: true,
-        autoplay: {
-          delay: 3000,
-          disableOnInteraction: false
-        },
-        pagination: { el: ".swiper-pagination" }
-      });
-      setSliderSwiper(sliderSwiper);
-    }
-  },[bannerList.length,sliderSwiper]);
+    const [sliderSwiper, setSliderSwiper] = useState(null);
+    const { bannerList } = props;
+    useEffect(() => {
+        if (bannerList.length && !sliderSwiper) {
+            let sliderSwiper = new Swiper(".slider-container", {
+                loop: true,
+                autoplay: {
+                    delay: 3000,
+                    disableOnInteraction: false,
+                },
+                pagination: { el: ".swiper-pagination" },
+            });
+            setSliderSwiper(sliderSwiper);
+        }
+    }, [bannerList.length, sliderSwiper]);
 
-  return(
-      <SliderContainer>
-        
-      </SliderContainer>
-  )
+    return <SliderContainer></SliderContainer>;
 }
